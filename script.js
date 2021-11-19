ce0caaef28332c527143e8bedf6b4d577c14ced6
Mohamud <mohamudhajji29@gmail.com>
2021-11-19 18:29:38 +0300

    adding submit function

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-$("#designimg").click(function() {
-    $(this).hide();
-    $("#designtxt").removeClass("designtxt");
-  });
-  $("#devimg").click(function() {
-    $(this).hide();
-    $("#devtxt").removeClass("devtxt");
-  });
+$(document).ready(function() {
+
+    $("#design").click(function(){
+      $("#design-showing").toggle();
+      $(".hide-design-image").toggle();
+    })
   
-  $("#prodimg").click(function() {
-    $(this).hide();
-    $("#prodtxt").removeClass("prodtxt");
-  });
+    $("#development").click(function(){
+      $("#development-showing").toggle();
+      $(".hide-development-image").toggle();
+    })
   
-  $("#designtxt").click(function() {
-    $("#designimg").show();
-    $("#designtxt").addClass("designtxt");
-  });
+    $("#productManagement").click(function(){
+      $("#product-management-showing").toggle();
+      $(".hide-product-management").toggle();
+    })
   
-  $("#devtxt").click(function() {
-    $("#devimg").show();
-    $("#devtxt").addClass("devtxt");
+    //Insert white box upo hover
+    $(".Hover").hover(function(){
+      $(this).animate({opacity:'1'});
+    },
+    function(){
+      $(this).animate({opacity:'0'});
+    })
+  $('.submit').click(function () {
+          var Name = $('#mce-FNAME').val();
+          var Email = $('#mce-EMAIL').val();
+          var Message = $('#mce-MESSAGE');
+          var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+          if (Name == '' || Email == '' || Message == '') {
+              alert('Please make sure you have filled in the form correctly!');
+          } else {
+              alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+          }
+    });
   });
-  
-  $("#prodtxt").click(function() {
-    $("#prodimg").show();
-    $("#prodtxt").addClass("prodtxt");
-  });
-  
-  $("#pro2").hover(function() {
-      console.log("hovered")
-      $("this").addClass("mask");
-  });
-  
